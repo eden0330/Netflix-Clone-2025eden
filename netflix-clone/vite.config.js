8bf75c3466f84caa9d834e06d5c2b3f4ec84cf51
eden0330 <edadani1415@gmail.com>
2025-12-10 14:50:54 +0300

    changed the base path

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/netflix-practice-ui-Clone-2025eden/",
+  base: "./",
 });
+// /netflix-practice-ui-Clone-2025eden/;
